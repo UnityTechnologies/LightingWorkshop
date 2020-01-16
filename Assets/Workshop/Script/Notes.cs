d9648cf38e6ef8de60c8f016fc4b3e8b18fd6f67
narendraChaudharyUnity <54363361+narendraChaudharyUnity@users.noreply.github.com>
2020-01-16 21:04:01 +0530

    first complete commit

---
+++ Assets/Workshop/Script/Notes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is just to store notes in editor for easy reference.
+/// </summary>
+public class Notes : MonoBehaviour
+{
+	[TextArea(10 , 14)]
+	[SerializeField]
+	string  note;
+    
+}
